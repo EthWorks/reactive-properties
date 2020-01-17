0da92c395da3ed7f11c1912ab566e807bb0c2bf6
Marik-D <maretskii@gmail.com>
2020-01-17 15:42:15 +0100

    Finilize dedupBy

--- src/operators/dedupBy.ts
+++ src/operators/dedupBy.ts
@@ -1,24 +1,19 @@
 import { Property, Callback } from "../Property"
 
 export function dedupBy <T> (eq: (a: T, b: T) => boolean) {
-  return (source: Property<T>) => {
-    let hasPrevious = false
-    let previous: T | undefined = undefined
+  return (source: Property<T>) => new class extends Property<T> {
+    get (): T {
+      return source.get()
+    }
 
-    return new class extends Property<T> {
-      get (): T {
-        return source.get()
-      }
-  
-      subscribe (cb: Callback): Callback {
-        return source.subscribe(() => {
-          if(!hasPrevious || !eq(previous!, source.get())) {
-            previous = source.get()
-            hasPrevious = true
-            cb()
-          }
-        })
-      }
+    subscribe (cb: Callback): Callback {
+      let previous = source.get()
+      return source.subscribe(() => {
+        if(!eq(previous, source.get())) {
+          previous = source.get()
+          cb()
+        }
+      })
     }
   }
 }
